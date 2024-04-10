2a9c225378d02faf522109b952e485b244374ed3
AstralArchitect <154975712+AstralArchitect@users.noreply.github.com>
2024-04-10 15:05:51 +0200

    Update main.c

--- Code Source/Recherche_nombresPremiers/main.c
+++ Code Source/Recherche_nombresPremiers/main.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
 #define MAX_SIZE 90000000
 
 int nombresPremiers = 1;
-int fin = 1000;
-int liste[MAX_SIZE] = {2};
+int fin;
 
 bool estPremier(int n) {
     for (int i = 2; i * i <= n; i++) {
         if (n % i == 0){
-            return false;
-        }
-        else if(n % 2 == 0){
             return false;
         }
     }
@@ -23,22 +20,21 @@ int main() {
     printf("Combien de nombres premiers voulez-vous chercher ? (max: 49 999 999)");
     scanf("%d", &fin);
     if (fin < MAX_SIZE) {
-        while (nombresPremiers >= fin) {
-        int num = start;
-        start++;
+	int num = 3;
+        while (nombresPremiers <= fin) {
+            num++;
         
-        if (estPremier(num)) {
-            if (*NombresPremiers < fin) {
-                liste[*NombresPremiers] = num;
-                (*NombresPremiers)++;
-            }
-        }
-				}
+            if (estPremier(num)) {
+                liste[nombresPremiers] = num;
+                nombresPremiers++;
+	    }
+	}
     } else {
-        printf("Veuillez entrer un nombre plus petit.");
+	printf("\e[0;31m");
+        printf("Erreur! Veuillez entrer un nombre plus petit.\n");
+	printf("\e[0;37m");
         exit(EXIT_FAILURE);
     }
 
     return 0;
 }
-
