85b75dc68d9b672af83dde618aea437020373f8e
Astral Architect <matthieu.jlp.poncet@gmail.com>
2024-10-13 18:42:03 +0200

    optimisation update

--- src/search.c
+++ src/search.c
@@ -3,93 +3,91 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include <pthread.h>
+
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
-#include <mutexManager.h>
-
-// modifier selon le nombre de coeur de votre processeur
+// ne pas modifier
 const unsigned char nb_threads = 2;
 
 typedef struct 
 {
     unsigned long fin;
-    MutexedVar *NombresPremiersTrouves;
+    unsigned long *NombresPremiersTrouves;
     unsigned long *liste;
     int stride;
     int tid;
 } ThreadInfo;
 
-bool estPremier(unsigned long n)
-{
-    if (n % 2 == 0)
-    {
+pthread_mutex_t mutex;
+
+// https://code.letuscrack.com/prime-numbers-ranges/
+bool estPremier(int n) {
+    if (n <= 1) {
         return false;
     }
-    
-    for (unsigned long j = 3; j * j <= n; j += 2) {
-        if (n % j == 0){
+    if (n <= 3) {
+        return true;
+    }
+    if (n % 2 == 0 || n % 3 == 0) {
+        return false;
+    }
+    int i = 5;
+    while (i * i <= n) {
+        if (n % i == 0 || n % (i + 2) == 0) {
             return false;
         }
+        i += 6;
     }
     return true;
 }
 
-void *search(void *argv)
-{
-    ThreadInfo *pointer = argv;
-    ThreadInfo threadInfo = *pointer;
-    while (true)
-    {
-        if (estPremier(threadInfo.tid))
-        {
-            threadInfo.liste[threadInfo.NombresPremiersTrouves->var] = threadInfo.tid;
-            increase(threadInfo.NombresPremiersTrouves, 1);
+void *search(void *argv) {
+    ThreadInfo *threadInfo = (ThreadInfo *)argv;
+
+    while (*threadInfo->NombresPremiersTrouves < threadInfo->fin) {
+        if (estPremier(threadInfo->tid)) {
+            pthread_mutex_lock(&mutex);
+            threadInfo->liste[*threadInfo->NombresPremiersTrouves] = threadInfo->tid;
+            (*threadInfo->NombresPremiersTrouves)++;
+            pthread_mutex_unlock(&mutex);
         }
 
-        threadInfo.tid += threadInfo.stride;
-        
-        if(threadInfo.NombresPremiersTrouves->var >= threadInfo.fin)
-        {
-            break;
-        }
+        threadInfo->tid += threadInfo->stride;
     }
-    return EXIT_SUCCESS;
+
+    return NULL;
 }
 
 unsigned long *find(unsigned long fin) {
     // variables
-    unsigned long *liste;
-    MutexedVar nombresPremiers;
-    initVar(&nombresPremiers, 1);
+    unsigned long *primes;
+    unsigned long numPrimesFound = 0;
 
     // allocation dynamique de mémoire sur le host
-    liste = (unsigned long*)malloc(fin * sizeof(unsigned long));
-    if (liste == NULL)
+    primes = (unsigned long*)malloc(fin * sizeof(unsigned long));
+    if (primes == NULL)
     {
         return NULL;
     }
-    liste[0] = 2;
 
     // parametrage des threads
-    ThreadInfo threads[nb_threads];
     pthread_t t[nb_threads];
-    int actualTid = 3;
-    for (unsigned char i = 0; i < nb_threads; i++)
-    {
-        threads[i].fin = fin;
-        threads[i].liste = liste;
-        threads[i].NombresPremiersTrouves = &nombresPremiers;
-        threads[i].stride = nb_threads;
-        threads[i].tid = actualTid;
-        actualTid++;
-    }
+    ThreadInfo threadInfos[nb_threads];
 
-    // recherche
+    pthread_mutex_init(&mutex, NULL);
+
     for (unsigned char i = 0; i < nb_threads; i++)
     {
-        pthread_create(&t[i], NULL, search, (void*)&threads[i]);
+        threadInfos[i].fin = fin;
+        threadInfos[i].liste = primes;
+        threadInfos[i].NombresPremiersTrouves = &numPrimesFound;
+        threadInfos[i].stride = nb_threads;
+        threadInfos[i].tid = i;
+        
+        pthread_create(&t[i], NULL, search, &threadInfos[i]);
     }
     
     // wait for threads finish
@@ -97,6 +95,8 @@ unsigned long *find(unsigned long fin) {
     {
         pthread_join(t[i], NULL);
     }
+
+    pthread_mutex_destroy(&mutex);
     
-    return liste;
+    return primes;
 }
